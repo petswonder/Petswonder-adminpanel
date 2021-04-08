da1615043f815dd3569c1ddb20e403952de56c64
jeswanthv <jeswanthv001@gmail.com>
2021-04-08 14:18:10 +0530

    added service section

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Routings from './components/Routings'
+import Routings from './components/Routings';
 
-import  './../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import './../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
-;
-
-ReactDOM.render(<Routings/>, document.getElementById('root')
-);
+ReactDOM.render(<Routings />, document.getElementById('root'));
